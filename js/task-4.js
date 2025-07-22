998856aecb940571aec5bb2352a2c371ce6463b9
Andrew <andrusha2349@gmail.com>
2025-07-22 22:06:36 +0300

    did homework5

---
+++ js/task-4.js
@@ -0,0 +1,50 @@
+const getTotalBalanceByGender = (users, gender) => {
+    let total_balance = 0
+    const balance_arr = users.filter(user => user.gender === gender).map(user => user.balance)
+    for (let balance of balance_arr) {
+        total_balance+=balance
+    }
+    return total_balance
+}
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
